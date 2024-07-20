699705663325492a08cb4c107a3e7267a2686d86
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2024-07-20 18:40:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -1,18 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		long x = sc.nextLong();
-
+		int num = Integer.parseInt(br.readLine());
 		long sum = 0;
-		for (int i = 1; i < x; i++) {
+
+		for (int i = 0; i < num; i++) {
 			sum += i;
 		}
 
-		System.out.println(sum);
-		System.out.println(2);
+        sb.append(sum).append('\n').append(2);
+		System.out.println(sb);
 	}
+
 }
