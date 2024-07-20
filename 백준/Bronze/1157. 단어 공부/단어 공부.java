066b7caaf5974fe5c2f240c88bf251cc9adbd7a2
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2024-07-20 11:11:13 +0900

    [Bronze I] Title: 단어 공부, Time: 384 ms, Memory: 36828 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,15 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//		StringBuilder answer = new StringBuilder();
-
-		String str = sc.nextLine().toUpperCase();
+		String str = br.readLine().toUpperCase();
 
 		Map<Character, Integer> map = new HashMap<>();
 
@@ -18,18 +19,18 @@ public class Main {
 			map.put(c, map.getOrDefault(c, 0) + 1);
 		}
 
-		Integer i = map.values().stream().max(Integer::compareTo).orElse(0);
+		Integer max = map.values().stream().max(Integer::compareTo).orElse(0);
 
-		Map<Character, Integer> second = new HashMap<>();
+		Map<Character, Integer> secondMap = new HashMap<>();
 
 		map.forEach((key, value) -> {
-			if (value.equals(i)) {
-				second.put(key, value);
+			if (value.equals(max)) {
+				secondMap.put(key, value);
 			}
 		});
 
-		if (second.size() == 1) {
-			System.out.println(second.keySet().iterator().next());
+		if (secondMap.size() == 1) {
+			System.out.println(secondMap.keySet().iterator().next());
 		} else {
 			System.out.println("?");
 		}
