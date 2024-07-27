6237856dbbd9b363da50333a577cb1007393e742
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2024-07-27 12:19:56 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 112 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -12,36 +12,41 @@ public class Main {
 
 		int count = Integer.parseInt(br.readLine());
 
+		/* 조합에서 종류가 n 개면 (n1+1) * (n2+1) * (n3+1) 이 조합의 수 */
+
 		for (int i = 0; i < count; i++) {
 
 			Map<String, Integer> map = new HashMap<>();
-			int clothCount = Integer.parseInt(br.readLine());
+			int costumeCount = Integer.parseInt(br.readLine());
 
-			for (int j = 0; j < clothCount; j++) {
+			for (int j = 0; j < costumeCount; j++) {
 				String[] arr = br.readLine().split(" ");
-
 				map.put(arr[1], map.getOrDefault(arr[1], 0) + 1);
 			}
 
 			if (map.size() > 1) {
-				int combinationCount = 1;
-				for (Integer value : map.values()) {
-					combinationCount *= (value + 1);
+				int sum = 1;
+
+				for (int value : map.values()) {
+					sum *= value + 1;
 				}
-				sb.append(combinationCount - 1).append('\n');
+
+				sb.append(sum - 1).append('\n');
 			} else {
 				if (map.size() == 1) {
 					int value = 0;
-					for (Integer v : map.values()) {
-						value = v;
+
+					for (Integer val : map.values()) {
+						value = val;
 					}
-					sb.append(value).append('\n');
+					
+					sb.append(value);
 				} else {
-					sb.append(0).append('\n');
+					sb.append(0);
 				}
+				sb.append('\n');
 			}
 		}
-
 		System.out.print(sb);
 	}
 }
