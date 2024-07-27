ac9749033d9999ea1911b3c9424cdebc309ce28e
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2024-07-27 14:50:05 +0900

    [Gold III] Title: 두 배열의 합, Time: 628 ms, Memory: 98272 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -11,49 +11,55 @@ public class Main {
 
 		int T = Integer.parseInt(br.readLine());
 
-		// 합, 갯수
-		Map<Integer, Integer> aMap = new HashMap<>();
-		Map<Integer, Integer> bMap = new HashMap<>();
-
-		int A = Integer.parseInt(br.readLine());
-		String[] arr = br.readLine().split(" ");
-
-		input(A, arr, aMap, T);
-
-		int B = Integer.parseInt(br.readLine());
-		arr = br.readLine().split(" ");
-		input(B, arr, bMap, T);
+		Map<Integer, Integer> firstMap = getArrayMap(Integer.parseInt(br.readLine()), br.readLine().split(" "));
+		Map<Integer, Integer> secondMap = getArrayMap(Integer.parseInt(br.readLine()), br.readLine().split(" "));
 
 		long answer = 0;
+		for (Map.Entry<Integer, Integer> entry : firstMap.entrySet()) {
 
-		for (Map.Entry<Integer, Integer> entryA : aMap.entrySet()) {
+			/*
+			 * 1. 목표치인 T에서 firstMap의 현재 키값을 빼면 필요한 값을 알 수 있음
+			 * -> T(5) - map.getKey(1) = 4
+			 * 2. secondMap 에 '4' 라는 key 값이 존재하는지 확인
+			 * 3. 존재한다면 곱해서 answer에 저장
+			 * -> ex) 1312 / 1312 일 때
+			 *      firstMap : 1=1, 2=1, 3=2, 4=2, 5=1, 6=1, 7=1
+			 *      secondMap : 1=1, 2=1, 3=2, 4=2, 5=1, 6=1, 7=1
+			 * -> T - key = 1 일때 : 1+3 +1, 3+1 +1, 1 +1+3, 1 +3+1 해서 총 4개, 2*2 개.
+			 * -> A[1] + A[2] + B[1], A[2] + A[3] + B[1], A[3] + B[1] + B[2], A[3] + B[2] + B[3] 임.
+			 * */
+			int otherKey = T - entry.getKey();
+			int otherValue = 0;
 
-			int otherKey = T - entryA.getKey();
-			Integer otherValue = bMap.get(otherKey);
+			if (secondMap.containsKey(otherKey)) {
+				otherValue = secondMap.get(otherKey);
+			}
 
-            if(otherValue != null) {
-			answer += ((long) entryA.getValue() * otherValue);
-            }
+			answer += (long) entry.getValue() * otherValue;
 		}
 
 		System.out.println(answer);
 	}
 
-	private static void input(int size, String[] arr, Map<Integer, Integer> map, int T) {
+	private static Map<Integer, Integer> getArrayMap(int length, String[] arr) {
+		Map<Integer, Integer> arrayMap = new HashMap<>();
 
-		for (int i = 0; i < size; i++) {
+		/*
+		 * 목표 : A[] = {1,3,1,2} 가 들어오면 맵에 부배열을 저장하기
+		 * -> A 의 부배열은 1,2,3,1+3,3+1,1+2 (연속된 숫자의 합)
+		 * -> 저장되는 형태는 1=1, 2=1, 3=2 (3,1+2) , 4=2 (1+3,3+1)
+		 * */
+		for (int i = 0; i < length; i++) {
 
 			int sum = 0;
 
-			for (int j = i; j < size; j++) {
-
+			for (int j = i; j < length; j++) {
 				sum += Integer.parseInt(arr[j]);
-				map.put(sum, map.getOrDefault(sum, 0) + 1);
 
+				arrayMap.put(sum, arrayMap.getOrDefault(sum, 0) + 1);
 			}
-
 		}
 
+		return arrayMap;
 	}
-
 }
