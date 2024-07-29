ecd8d9df56c1aa98a2dacf5b2ded1dfe76be8346
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2024-07-29 15:47:57 +0900

    [Gold II] Title: 보석 도둑, Time: 1576 ms, Memory: 122560 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,48 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
 		String[] split = br.readLine().split(" ");
-		int count = Integer.parseInt(split[0]);
+		int jewelCount = Integer.parseInt(split[0]);
+		int bagCount = Integer.parseInt(split[1]);
 
-		List<Integer> list = new ArrayList<>();
-		int[][] max = new int[count][2];
-
-		for (int i = 0; i < count; i++) {
-			String[] arr = br.readLine().split(" ");
-			max[i] = new int[]{Integer.parseInt(arr[0]), Integer.parseInt(arr[1])};
-		}
-
-		for (int i = 0; i < Integer.parseInt(split[1]); i++) {
-			list.add(Integer.parseInt(br.readLine()));
-		}
-
-		long answer = 0;
-		list.sort(Integer::compareTo);
-		Arrays.sort(max, (a, b) -> {
+		PriorityQueue<Integer> bagList = new PriorityQueue<>();
+		PriorityQueue<Integer> min = new PriorityQueue<>((a, b) -> b - a);
+		PriorityQueue<int[]> max = new PriorityQueue<>((a, b) -> {
 			if (a[0] == b[0]) {
 				return b[1] - a[1];
 			}
 			return a[0] - b[0];
 		});
 
-		PriorityQueue<Integer> min = new PriorityQueue<>((a, b) -> b - a);
-		int index = 0;
+		for (int i = 0; i < jewelCount; i++) {
+			st = new StringTokenizer(br.readLine());
+			max.offer(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+		}
 
-		for (int i = 0; i < list.size(); i++) {
+		for (int i = 0; i < bagCount; i++) {
+			bagList.offer(Integer.parseInt(br.readLine()));
+		}
 
-			while (index < max.length && max[index][0] <= list.get(i)) {
-				min.offer(max[index][1]);
-				index++;
+		long answer = 0;
+
+		for (int i = 0; i < bagCount; i++) {
+
+			if (!bagList.isEmpty()) {
+				int bag = bagList.poll();
+
+				while (!max.isEmpty() && bag >= max.peek()[0]) {
+					min.offer(max.poll()[1]);
+				}
 			}
 
 			if (!min.isEmpty()) {
