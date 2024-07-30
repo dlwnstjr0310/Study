515c452e764e593e65eff9948e332ad7d9f5acfb
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2024-07-30 19:23:51 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 156 ms, Memory: 16256 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -1,54 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+
+	static int[][] map;
+	static boolean[][] isVisited;
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		/*
-		 * 1. 쩰리는 정사각형 외부로 나가는경우 죽는다
-		 * 2. 시작점은 항상 왼쪽 위 (0,0)
-		 * 3. 우하단으로만 이동 가능
-		 * 4. 6x6 일 경우 5,5 에 도착하면 쩰리의 승리
-		 * 5. 한 번에 이동할 수 있는 칸은 정확히 현재 칸에 적혀있는 숫자만큼. 초과나 그 미만으로 이동할 수 없다.
-		 * */
 		int num = Integer.parseInt(br.readLine());
-		int[][] map = new int[num][num];
-		boolean[][] isVisited = new boolean[num][num];
+
+		map = new int[num][num];
+		isVisited = new boolean[num][num];
 
 		for (int i = 0; i < num; i++) {
-			map[i] = Arrays.stream(br.readLine().split(" "))
-					.mapToInt(Integer::parseInt)
-					.toArray();
+			String[] split = br.readLine().split(" ");
+			for (int j = 0; j < num; j++) {
+				map[i][j] = Integer.parseInt(split[j]);
+			}
 		}
 
-		dfs(0, 0, map, isVisited);
+		dfs(0, 0);
+
 		System.out.println("Hing");
 	}
 
-	private static void dfs(int row, int col, int[][] map, boolean[][] isVisited) {
+	static void dfs(int row, int col) {
 
-		if (map[row][col] == -1) {
+		isVisited[row][col] = true;
+		int step = map[row][col];
+
+		if (step == -1) {
 			System.out.println("HaruHaru");
 			System.exit(0);
 		}
-		if (isVisited[row][col] || map[row][col] == 0) {
-			return;
-		} else {
-			int nc = col + map[row][col];
-			int nr = row + map[row][col];
 
-			if (nc < map.length && !isVisited[row][nc]) {
-				dfs(row, nc, map, isVisited);
-			}
+		int nr = row + step;
+		int nc = col + step;
 
-			if (nr < map.length && !isVisited[nr][col]) {
-				dfs(nr, col, map, isVisited);
-			}
+		if (nr < map.length && !isVisited[nr][col]) {
+			dfs(nr, col);
 		}
-		isVisited[row][col] = true;
+		if (nc < map.length && !isVisited[row][nc]) {
+			dfs(row, nc);
+		}
+
 	}
 }
