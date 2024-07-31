58d98f4a8b20cdb4cf9cc8d4af71cacb042121ff
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2024-07-31 10:08:21 +0900

    [Silver IV] Title: 보물, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -9,15 +10,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int num = Integer.parseInt(br.readLine());
-		String[] arr1 = br.readLine().split(" ");
-		String[] arr2 = br.readLine().split(" ");
 
+		/*
+		 * 위 배열의 최소값 * 아래 배열의 최대값 : S 의 최소값
+		 * */
 		PriorityQueue<Integer> min = new PriorityQueue<>();
 		PriorityQueue<Integer> max = new PriorityQueue<>((a, b) -> b - a);
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for (int i = 0; i < num; i++) {
-			min.offer(Integer.parseInt(arr1[i]));
-			max.offer(Integer.parseInt(arr2[i]));
+			min.offer(Integer.parseInt(st.nextToken()));
+		}
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < num; i++) {
+			max.offer(Integer.parseInt(st.nextToken()));
 		}
 
 		int sum = 0;
@@ -25,7 +34,7 @@ public class Main {
 		for (int i = 0; i < num; i++) {
 			sum += min.poll() * max.poll();
 		}
-		
-		System.out.println(sum);
+
+		System.out.print(sum);
 	}
 }
